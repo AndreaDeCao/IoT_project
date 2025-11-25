b930a213fc07195691a7aa5b9ade71b9c55adffd
Andrea <95381046+AndreaDeCao@users.noreply.github.com>
2025-11-25 18:04:37 +0100

    Finished the entire section relating to the website, improving the photos and descriptions.

--- docs/script.js
+++ docs/script.js
@@ -19,3 +19,44 @@ document.getElementById("temp").addEventListener("click", () => {
 }
 showWelcomeMessage();*/
 
+document.addEventListener("DOMContentLoaded", () => {
+  const slider = document.getElementById("slider");
+  const slides = slider.children;
+  const total = slides.length;
+  document.getElementById("prev").style.display = "none";
+  document.getElementById("next").style.display = "none";
+
+  let index = 0;
+
+  // Imposta la larghezza totale del contenitore (importantissimo!)
+  slider.style.width = `${total * 100}%`;
+
+  // Ogni immagine deve occupare la stessa parte dello slider
+  for (let s of slides) {
+    s.style.width = `${100 / total}%`;
+  }
+  document.getElementById("slider-container").onmouseenter = () => {
+    document.getElementById("prev").style.display="block";
+    document.getElementById("next").style.display = "block";
+  }
+  document.getElementById("slider-container").onmouseleave = () => {
+    document.getElementById("prev").style.display="none";
+    document.getElementById("next").style.display = "none";
+  }
+
+  function updateSlider() {
+    slider.style.transform = `translateX(-${index * (100 / total)}%)`;
+  }
+
+  document.getElementById("next").onclick = () => {
+    index = (index + 1) % total;
+    updateSlider();
+  };
+
+  document.getElementById("prev").onclick = () => {
+    index = (index - 1 + total) % total;
+    updateSlider();
+  };
+});
+
+
